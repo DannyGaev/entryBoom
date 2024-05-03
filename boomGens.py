e44f6e58ac905d70c1a4577bc93040ff3b4157a6
OakTree-42 <DannyGaev@gmail.com>
2024-05-02 21:03:34 -0500

    Updated code to account for case-sensitive answers

--- boomGens.py
+++ boomGens.py
@@ -1,6 +1,6 @@
 import random
 import string
-import names 
+import names
 import json
 
 
@@ -10,7 +10,7 @@ def genNumber():
     return numbers[random.randint(0, len(numbers)-1)]
 
 
-def genInput(rangeOf): 
+def genInput(rangeOf):
     return ''.join(random.choice(string.ascii_uppercase + string.digits)
                    for _ in range(rangeOf))
 
@@ -155,52 +155,75 @@ def genNames(count):
     return allNames
 
 
-def genPayload(entryIds, categories, session, user_agent):
+def genPayload(entryIds, categories, answers, session, user_agent):
     payload = {}
     for x in range(0, len(categories)):
         rand = random.randint(1, 2) == 1
         c = categories[x].lower()
         el = ""
-        if "name" in c:
-            el = genNames(1)[0]
-        elif "local" in c or "mailing" in c or "address" in c or "state" in c or "city" in c:
-            el = genInput(7)
-        elif "zip" in c:
-            el = genInput(5)
-        elif "phone" in c:
-            el = genNumber()
-        elif "gender" in c:
-            el = "Female" if rand else "Male"
-        elif "do you" in c or "are you" in c or  "i know how to" in c or "how to" in c:
-            el = "Yes" if rand else "No"
-        elif "age" in c:
-            el = "21"
-        elif "occupation" in c or "job" in c or "position" in c:
-            el = ''.join(genJob())
-        elif "e-mail" in c or "email" in c or "mail" in c:
-            found = False
-            while not found:
-                response = session.get(
-                    "https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1", headers={'User-Agent': user_agent})
-                try:
-                    el = json.loads(response.text)[0]
-                    found = True
-                except:
-                    pass
-        elif c=="":
-            el = "Option 1"
-        elif "comments" in c:
-            rand2 = random.randint(1, 5)
-            match rand2:
-                case 1:
-                    el = "We have collected information about your computer and network -- it will be reported to the proper authorities. Good luck."
-                case 2:
-                    el = "How do I use the service?"
-                case 3:
-                    el = "I had a few questions abut the pay ! Contact me at my other email at  supposedChi1ck3n@proton.me, thank yuo!"
-                case 4:
-                    el = "As a {job}, how would I go about quitting and transferring to you? thanks!".format(job=''.join(genJob()))
-                case 5:
-                    el = "."
+        if (answers[x][0] > 0):
+            rand2 = random.randint(0, len(answers[x][1])-1)
+            willBe = answers[x][1][rand2]
+            try:
+                willBe = willBe.replace("\\", "").replace('"', '')
+            except:
+                continue
+            el = willBe
+        else:
+            if "name" in c:
+                el = genNames(1)[0]
+            elif "local" in c or "mailing" in c or "address" in c or "state" in c or "city" in c:
+                el = genInput(7)
+            elif "zip" in c:
+                el = genInput(5)
+            elif "phone" in c:
+                el = genNumber()
+            elif "gender" in c:
+                el = "Female" if rand else "male"
+            elif "do you" in c or "are you" in c or "i know how to" in c or "how to" in c:
+                el = "Yes" if rand else "No"
+            elif "age" in c:
+                el = "21"
+            elif "occupation" in c or "job" in c or "position" in c:
+                el = ''.join(genJob())
+            elif "pass" in c or "password" in c:
+                el = genInput(15)
+            elif "School Attended" in c or "school attended" in c:
+                el = genNames(1)[0]
+            elif "year" in c:
+                el = genInput(4)
+            elif "time" in c:
+                rand2 = random.randint(1, 3)
+                match rand2:
+                    case 1:
+                        el = "Morning"
+                    case 2:
+                        el = "Afternoon"
+                    case 3:
+                        el = "Evening"
+            elif "e-mail" in c or "email" in c or "mail" in c:
+                found = False
+                while not found:
+                    response = session.get(
+                        "https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1", headers={'User-Agent': user_agent})
+                    try:
+                        el = json.loads(response.text)[0]
+                        found = True
+                    except:
+                        pass
+            elif "comments" in c:
+                rand2 = random.randint(1, 5)
+                match rand2:
+                    case 1:
+                        el = "We have collected information about your computer and network -- it will be reported to the proper authorities. Good luck."
+                    case 2:
+                        el = "How do I use the service?"
+                    case 3:
+                        el = "I had a few questions abut the pay ! Contact me at my other email at  supposedChi1ck3n@proton.me, thank yuo!"
+                    case 4:
+                        el = "As a {job}, how would I go about quitting and transferring to you? thanks!".format(
+                            job=''.join(genJob()))
+                    case 5:
+                        el = "."
         payload["entry."+entryIds[x]] = el
     return payload
