949b717a6474f2ec856050b3b920486046e6c7fa
OakTree-42 <DannyGaev@gmail.com>
2024-07-05 14:05:31 -0500

    Updated reformatting process

--- entryBoom.py
+++ entryBoom.py
@@ -4,75 +4,32 @@ import time
 import threading
 import requests
 import argparse
-from formScrape import * 
+from formScrape import *
 from boomGens import *
 from missile import *
 
-#             _                ____   ___   ___  __  __
-#            | |              |  _ \ / _ \ / _ \|  \/  |
-#   ___ _ __ | |_ _ __ _   _  | |_) | | | | | | | \  / |
-#  / _ \ '_ \| __| '__| | | | |  _ <| | | | | | | |\/| |
-# |  __/ | | | |_| |  | |_| |_| |_) | |_| | |_| | |  | |
-#  \___|_| |_|\__|_|   \__, (_)____/ \___/ \___/|_|  |_|
-#                       __/ |
-#                      |___/
 
-
-def launch(URL, payloads, verbose, nickname=None):
-    if nickname is None:
-        nickname = URL
-    start = time.time()
-    URL = URL.replace("[.]", ".")
-    if "formResponse" not in URL:
-        print("Reformatting the URL...")
-        r = requests.get(URL)
-        URL = r.url
-        URL = URL[0:URL.find("viewform")]+"formResponse"
-
-    entryIds, categories, answers = findFields(URL)
-    createExample(entryIds, categories, answers)
-    print(f"\nSending {payloads} payloads of randomly generated data to {nickname}...")
-    threads = []
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    args.verbose = True
-    if not verbose:
-        args.verbose = False
-    for _ in range(payloads):
-        posting = threading.Thread(
-            target=booming, args=(URL, entryIds, categories, answers, args, _))
-        threads.append(posting)
-        posting.start()
-        time.sleep(0.1)
-    for thread in threads:
-        thread.join()
-    end = time.time()
-    getTable(start, end)
-
-if __name__ == '__main__':
-    ascii_banner = pyfiglet.figlet_format("entry.B00M", font="big")
-    colored_ascii_art = termcolor.colored(ascii_banner, color='blue')
-    print(colored_ascii_art)
-    parser = argparse.ArgumentParser(
-        description="Send thousands of POST requests to scammers' Google Forms")
-    parser.add_argument('-u', dest="url", type=str, help='URL of the form')
-    parser.add_argument('-n', dest="num", type=int,
-                        help='Number of requests')
-    parser.add_argument('-v', dest="verbose", default=False,
-                        action='store_true', help='Enable verbose mode')
-    args = parser.parse_args()
+def main(args):
     start = time.time()
     URL = str(args.url)
     URL = URL.replace("[.]", ".")
     payloads = args.num
 
+    numReformat = 1
+    if "http://" not in URL and "https://" not in URL:
+        print(f"Reformatting the URL... [{numReformat}]")
+        URL = "https://"+URL
+        print(f"Completed reformat [{numReformat}]")
+        numReformat += 1
     if "formResponse" not in URL:
-        print("Reformatting the URL...")
+        print(f"Reformatting the URL... [{numReformat}]")
         r = requests.get(URL)
         URL = r.url
         URL = URL[0:URL.find("viewform")]+"formResponse"
+        print(f"Completed reformat [{numReformat}]")
 
     entryIds, categories, answers = findFields(URL)
+
     createExample(entryIds, categories, answers)
     print(f"\nSending {
         payloads} payloads of randomly generated data to the form...")
@@ -87,3 +44,22 @@ if __name__ == '__main__':
         thread.join()
     end = time.time()
     getTable(start, end)
+
+
+if __name__ == '__main__':
+    try:
+        ascii_banner = pyfiglet.figlet_format("entry.B00M", font="big")
+        colored_ascii_art = termcolor.colored(ascii_banner, color='blue')
+        print(colored_ascii_art)
+        parser = argparse.ArgumentParser(
+            description="Send thousands of POST requests to scammers' Google Forms")
+        parser.add_argument('-u', dest="url", type=str, help='URL of the form')
+        parser.add_argument('-n', dest="num", type=int,
+                            help='Number of requests')
+        parser.add_argument('-v', dest="verbose", default=False,
+                            action='store_true', help='Enable verbose mode')
+        args = parser.parse_args()
+        main(args)
+    except Exception as e:
+        if "ERR_PROXY_CONNECTION_FAILED" in str(e):
+            print("OPEN A TOR BROWSER TAB BEFORE PROCEEDING")
