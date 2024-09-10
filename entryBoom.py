0a7005c6924163cdfc71ccaea7e771410484b8b8
OakTree-42 <DannyGaev@gmail.com>
2024-09-10 14:42:50 -0500

    Added payload feature

--- entryBoom.py
+++ entryBoom.py
@@ -3,13 +3,23 @@ import pyfiglet
 import time
 import threading
 import requests
-import argparse
+import argparse 
 from formScrape import *
 from boomGens import *
 from missile import *
+ 
+ascii_banner = pyfiglet.figlet_format("entry.B00M", font="big")
+colored_ascii_art = termcolor.colored(ascii_banner, color='blue')
+print(colored_ascii_art)
+
+class Payload():
+    def __init__(self, url, num, verbose):
+        self.url = url
+        self.num = num
+        self.verbose = verbose
 
 
-def main(args):
+def launch(args):
     start = time.time()
     URL = str(args.url)
     URL = URL.replace("[.]", ".")
@@ -23,7 +33,7 @@ def main(args):
         numReformat += 1
     if "formResponse" not in URL:
         print(f"Reformatting the URL... [{numReformat}]")
-        r = requests.get(URL)
+        r = requests.get(URL) 
         URL = r.url
         URL = URL[0:URL.find("viewform")]+"formResponse"
         print(f"Completed reformat [{numReformat}]")
@@ -48,9 +58,6 @@ def main(args):
 
 if __name__ == '__main__':
     try:
-        ascii_banner = pyfiglet.figlet_format("entry.B00M", font="big")
-        colored_ascii_art = termcolor.colored(ascii_banner, color='blue')
-        print(colored_ascii_art)
         parser = argparse.ArgumentParser(
             description="Send thousands of POST requests to scammers' Google Forms")
         parser.add_argument('-u', dest="url", type=str, help='URL of the form')
@@ -59,7 +66,7 @@ if __name__ == '__main__':
         parser.add_argument('-v', dest="verbose", default=False,
                             action='store_true', help='Enable verbose mode')
         args = parser.parse_args()
-        main(args)
+        launch(args)
     except Exception as e:
         if "ERR_PROXY_CONNECTION_FAILED" in str(e):
             print("OPEN A TOR BROWSER TAB BEFORE PROCEEDING")
